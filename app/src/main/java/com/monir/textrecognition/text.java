3b7d15b14c2f3945904aed4b27b1e69c8ff16dcd
z4zzaman <zzaman08@gmail.com>
2019-01-06 22:06:11 +0600

    initial commit

---
+++ app/src/main/java/com/monir/textrecognition/text.java
@@ -0,0 +1,18 @@
+package com.monir.textrecognition;
+
+/*
+ *  ****************************************************************************
+ *  * Created by : Md. Moniruzzaman Monir on 1/6/2019 at 9:52 PM.
+ *  * Email : zzaman08@gmail.com
+ *  *
+ *  * Purpose:
+ *  *
+ *  * Last edited by : Md. Moniruzzaman Monir on 1/6/2019.
+ *  *
+ *  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>
+ *  ****************************************************************************
+ */
+
+public class text {
+
+}
